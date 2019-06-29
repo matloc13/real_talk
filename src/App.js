c84ccf94efafbfa703c8a1df19220b54763420f6
Matthew Lockdall <mlockdall26@gmail.com>
2019-06-29 16:11:42 -0500

    getposts

--- src/App.js
+++ src/App.js
@@ -1,21 +1,49 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+let baseUrl = 'https://matloc13.github.io/real_talk/'
 
 class App extends Component{
+  state = {
+    blogPosts: []
+  }
+  getPosts = () => {
+    fetch(baseUrl + '/blogposts').then(data => data.json(), err => console.log(err)).then(parsedData => this.setState({
+      blogPosts: parsedData
+    }))
+  }
+  // handleChange = (event) => {
+  //   this.setState({
+  //     [event.target.id]: event.target.value
+  //   })
+  // }
+  componentDidMOunt = () => {
+    this.getPosts()
+  }
   render() {
-
 
   return (
     <div className="App">
       <h1>hello</h1>
 
-      <form onSubmit="">
+      <div className="displayPosts">
+        <ul>
+          {
+            this.state.blogPosts.map(blogPost =>
+              <li key={blogPost._id}>
+                {blogPost.title}
+              </li>
+            )
+          }
+        </ul>
+      </div>
+
+      {/* <form onSubmit="">
         <label htmlFor="title">Title</label>
-        <input type="text" id="title" onChange={}/>
+        <input type="text" id="title" onChange={this.handleChange}/>
         <label htmlFor="post">Post</label>
         <textarea name="post" id="post"></textarea>
-      </form>
+      </form> */}
 
     </div>
   );
