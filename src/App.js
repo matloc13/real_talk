3edf2032e3f3d5317d42fc917fe79764647ea70f
Matthew Lockdall <mlockdall26@gmail.com>
2019-06-29 14:56:28 -0500

    form created

--- src/App.js
+++ src/App.js
@@ -8,20 +8,15 @@ class App extends Component{
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hello</h1>
+
+      <form onSubmit="">
+        <label htmlFor="title">Title</label>
+        <input type="text" id="title" onChange={}/>
+        <label htmlFor="post">Post</label>
+        <textarea name="post" id="post"></textarea>
+      </form>
+
     </div>
   );
   }
