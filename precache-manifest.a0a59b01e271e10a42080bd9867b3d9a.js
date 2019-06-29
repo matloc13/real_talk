efa6aa43637bbcd93644bb1993eb64c485189fbe
Matthew Lockdall <mlockdall26@gmail.com>
2019-06-29 12:27:48 -0500

    Updates

---
+++ precache-manifest.a0a59b01e271e10a42080bd9867b3d9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83027f426c8fc503c344cf01f6a53ae5",
+    "url": "/real_talk/index.html"
+  },
+  {
+    "revision": "f803a5ee9cc08fb8b70d",
+    "url": "/real_talk/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/real_talk/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "f803a5ee9cc08fb8b70d",
+    "url": "/real_talk/static/js/main.4daf0a40.chunk.js"
+  },
+  {
+    "revision": "d57235e776b9e6ca1291",
+    "url": "/real_talk/static/js/runtime~main.2c182053.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/real_talk/static/media/logo.5d5d9eef.svg"
+  }
+]);
