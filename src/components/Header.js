c392dc16c330e3e97364b6e90404204ac7f83680
Matthew Lockdall <mlockdall26@gmail.com>
2019-07-06 12:20:49 -0500

    123

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,49 +1,52 @@
 
 import React, {Component} from 'react'
+import {Link} from 'react-router-dom'
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 import Login from './NewSession.js'
 import RegisterUser from './RegisterUser.js'
 
 class Header extends Component {
+
     handleSubmit = (event) => {
         event.preventDefault();
         fetch(this.props.baseURL+ '/blogposts', {
             method: 'DELETE'
         }).then(res => res.json())
         .then(resJson => {
-            this.setState({
 
-            })
         })
     }
 
     render () {
         return (
-                this.props.currentUser ?
+          <header>
+            {
+              this.props.currentUser ?
                 <nav className="navbar">
-
-                    <a className="btn" href="/"><h1>Real Talk</h1></a>
-                    <h4 className="mr-n5">{this.props.currentUser.username}</h4>
-                    <form onSubmit={this.handleSubmit}>
+                  <a className="btn" href="/"><h1>Real Talk</h1></a>
+                  <h4 className="mr-n5">{this.props.currentUser.username}</h4>
+                  <form onSubmit={this.handleSubmit}>
                     <input className="btn" type="submit" value="Logout" />
-                    </form>
+                  </form>
                 </nav>
               :
               <nav className="navbar">
                 <a className="btn" href="/"><h1>Real Talk</h1></a>
                 <Router>
+                  <Route path="/login" render={(props) => <Login {...props} baseURL={this.props.baseURL} handleChange={this.props.handleChange}/>}/>
 
-                    <Link to="/login">Login</Link>
-                    <Route path="/login" render={(props) => <Login {...props} baseURL={this.props.baseURL} handleChange={this.props.handleChange} username={this.props.username} password={this.props.password}/>}/>
+                  <Link to="/login">Login</Link>
 
+                  <Route path="/register" render={(props) => <RegisterUser {...props} handleChange={this.props.handleChange} baseURL={this.props.baseURL} addUser={this.props.addUser}/>}/>
 
-                    <Link to="/register">Register</Link>
-                    <Route path="/register" render={(props) => <RegisterUser {...props} handleChange={this.props.handleChange} baseURL={this.props.baseURL} addUser={this.props.addUser} name={this.props.name} username={this.props.username} password={this.props.password}/>}/>
+                  <Link to="/register">Register</Link>
+
 
                 </Router>
               </nav>
+              </nav>
             }
-          </header>
+              </header>
 
         )
     }
