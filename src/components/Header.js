ea36dd85cdf02f5f957a81b632bddc363c1755ce
Matthew Lockdall <mlockdall26@gmail.com>
2019-07-06 12:48:47 -0500

    moved bootstrap

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,12 @@
 
 import React, {Component} from 'react'
-import {Link} from 'react-router-dom'
+
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 import Login from './NewSession.js'
 import RegisterUser from './RegisterUser.js'
 
 class Header extends Component {
 
-<<<<<<< HEAD
-=======
   state = {
     user: {
       name: '',
@@ -17,16 +15,12 @@ class Header extends Component {
     }
   }
 
->>>>>>> 57ae8657359322b5b3971106744387036793ed48
     handleSubmit = (event) => {
         event.preventDefault();
         fetch(this.props.baseURL + '/sessions', {
             method: 'DELETE'
         }).then(res => res.json())
         .then(resJson => {
-<<<<<<< HEAD
-
-=======
           console.log(resJson)
             this.setState({
                 user: {
@@ -37,31 +31,21 @@ class Header extends Component {
             })
 
             // this.props.getUser()
->>>>>>> 57ae8657359322b5b3971106744387036793ed48
         })
     }
 
     render () {
         return (
           <header>
-<<<<<<< HEAD
-            {
-              this.props.currentUser ?
-                <nav className="navbar">
-                  <a className="btn" href="/"><h1>Real Talk</h1></a>
-                  <h4 className="mr-n5">{this.props.currentUser.username}</h4>
-                  <form onSubmit={this.handleSubmit}>
-=======
             {console.log(this.props.user)}
             {
-              // (this.props.user != null && this.props.user.username != '') 
+              // (this.props.user != null && this.props.user.username != '')
               this.props.user ?
                 <nav className="navbar">
 
-                    <a className="btn" href="/"><h1>Real Talk</h1></a>
-                    <h4 className="mr-n5">{this.props.user.username}</h4>
-                    <form onSubmit={this.handleSubmit}>
->>>>>>> 57ae8657359322b5b3971106744387036793ed48
+                  <a className="btn" href="/"><h1>Real Talk</h1></a>
+                  <h4 className="mr-n5">{this.props.user.username}</h4>
+                  <form onSubmit={this.handleSubmit}>
                     <input className="btn" type="submit" value="Logout" />
                   </form>
                 </nav>
@@ -71,12 +55,8 @@ class Header extends Component {
                 <Router>
                   <Route path="/login" render={(props) => <Login {...props} baseURL={this.props.baseURL} handleChange={this.props.handleChange}/>}/>
 
-<<<<<<< HEAD
                   <Link to="/login">Login</Link>
-=======
-                    <Link to="/login">Login</Link>
-                    <Route path="/login" render={(props) => <Login {...props} baseURL={this.props.baseURL} handleChange={this.props.handleChange} username={this.props.username} password={this.props.password} getUser={this.props.getUser}/>}/>
->>>>>>> 57ae8657359322b5b3971106744387036793ed48
+                  <Route path="/login" render={(props) => <Login {...props} baseURL={this.props.baseURL} handleChange={this.props.handleChange} username={this.props.username} password={this.props.password} getUser={this.props.getUser}/>}/>
 
                   <Route path="/register" render={(props) => <RegisterUser {...props} handleChange={this.props.handleChange} baseURL={this.props.baseURL} addUser={this.props.addUser}/>}/>
 
@@ -85,7 +65,7 @@ class Header extends Component {
 
                 </Router>
               </nav>
-              </nav>
+              
             }
               </header>
 
