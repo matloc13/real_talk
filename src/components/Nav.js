d81376893efecadabfbf02d146e9bb5d4291effa
Matthew Lockdall <mlockdall26@gmail.com>
2019-07-06 13:30:17 -0500

    merging basic comments

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,17 +1,17 @@
 import React from 'react'
 
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem } from 'reactstrap';
+// import {
+//   Collapse,
+//   Navbar,
+//   NavbarToggler,
+//   NavbarBrand,
+//   Nav,
+//   NavItem,
+//   NavLink,
+//   UncontrolledDropdown,
+//   DropdownToggle,
+//   DropdownMenu,
+//   DropdownItem } from 'reactstrap';
 
 import { Link } from 'react-router-dom'
 
